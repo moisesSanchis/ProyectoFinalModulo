d2f36a683759954b51dc45e05f5d4035f41d4b97
moises <moisessanchispla@gmail.com>
2023-05-13 19:21:27 +0200

    release_13_05_23

--- app/src/main/java/com/msp/proyectomoises/LoginActivity.kt
+++ app/src/main/java/com/msp/proyectomoises/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.msp.proyectomoises
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,45 +10,69 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
+import database.RecyclerSQLiteHelper
 import entities.User
 
-class LoginActivity : AppCompatActivity(){
-    private lateinit var etUserLogin:EditText
-    private lateinit var etPasswordLogin:EditText
-    private lateinit var btnLogin:Button
-    private lateinit var btnRegister:Button
-    private lateinit var  btnExit:ImageButton
-    private lateinit var user:User
+class LoginActivity : AppCompatActivity() {
+    private lateinit var etUserLogin: EditText
+    private lateinit var etPasswordLogin: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnRegister: Button
+    private lateinit var btnExit: ImageButton
+    lateinit var recyclerDB: RecyclerSQLiteHelper
 
-
+    @SuppressLint("Recycle")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        recyclerDB = RecyclerSQLiteHelper(this)
         etUserLogin = findViewById(R.id.etUserLogin)
         etPasswordLogin = findViewById(R.id.etPasswordLogin)
         btnLogin = findViewById(R.id.btnLogin)
         btnRegister = findViewById(R.id.btnRegisterLogin)
         btnExit = findViewById(R.id.ibtnExitLogin)
-        var userString:String =""
-        var pass:String =""
-        etUserLogin.addTextChangedListener{
-          userString = etUserLogin.text.toString()
+        var userString: String = ""
+        var pass: String = ""
+        etUserLogin.addTextChangedListener {
+            userString = etUserLogin.text.toString()
         }
-        etPasswordLogin.addTextChangedListener{
-            pass =  etPasswordLogin.text.toString()
+        etPasswordLogin.addTextChangedListener {
+            pass = etPasswordLogin.text.toString()
         }
 
         btnLogin.setOnClickListener {
-            if (userString == "moises" && pass == "1234"){
-                user = User("Moises", "1234",20, "moises@gmail.com")
-                val intentMain = Intent(this, MainActivity::class.java)
-                intentMain.putExtra("newUser", user)
-                startActivity(intentMain)
-
-
-            }else{
-                Toast.makeText(this, R.string.TxtErrorLogin, Toast.LENGTH_SHORT).show()
+            val readerDb: SQLiteDatabase = recyclerDB.readableDatabase
+            val cursor = readerDb.rawQuery("SELECT name, password,points, mail, image  FROM user", null)
+            val listUsers = ArrayList<User>()
+            var count = 0
+            if (cursor.moveToFirst()) {
+                do {
+                    val nameCursor: String = cursor.getString(0)
+                    val passwordCursor: String = cursor.getString(1)
+                    val mailCursor: String = cursor.getString(3)
+                    val pointsCursor: Int = cursor.getInt(2)
+                    val imageCursor: Int = cursor.getInt(4)
+                    val userActual = User(nameCursor, passwordCursor, pointsCursor, mailCursor, imageCursor)
+                    listUsers.add(userActual)
+                } while (cursor.moveToNext())
             }
+            listUsers.forEach{ user ->
+
+                if (userString == user.getName() && pass == user.getPassword()) {
+                    val intentMain = Intent(this, MainActivity::class.java)
+                    intentMain.putExtra("newUser", user)
+                    startActivity(intentMain)
+
+
+                } else {
+                    count++
+                }
+            }
+            if (listUsers.size==count){
+                Toast.makeText(this, R.string.TxtErrorLogin, Toast.LENGTH_SHORT).show()
+
+            }
+
 
         }
         btnRegister.setOnClickListener {
@@ -56,7 +82,6 @@ class LoginActivity : AppCompatActivity(){
         btnExit.setOnClickListener {
             finishAffinity()
         }
-
 
 
     }
--- app/src/main/java/com/msp/proyectomoises/RegisterActivity.kt
+++ app/src/main/java/com/msp/proyectomoises/RegisterActivity.kt
@@ -2,12 +2,17 @@ package com.msp.proyectomoises
 
 
 import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
+import database.RecyclerDao
+import database.RecyclerSQLiteHelper
 import entities.User
 
 class RegisterActivity : AppCompatActivity() {
@@ -15,35 +20,74 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var etUserPassword: EditText
     private lateinit var etUserRepeatPassword: EditText
     private lateinit var etUserMail: EditText
-    private lateinit var btnRegister:Button
+    private lateinit var btnRegister: Button
     lateinit var newUser: User
+    lateinit var recyclerDB: RecyclerSQLiteHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        var userName:String = ""
-        var userPassword:String = ""
-        var userRepeatPassWord:String = ""
-        var userMail:String = ""
-        etUserName.addTextChangedListener{
+        var userName: String = ""
+        var userPassword: String = ""
+        var userRepeatPassWord: String = ""
+        var userMail: String = ""
+        recyclerDB = RecyclerSQLiteHelper(this)
+        etUserName = findViewById(R.id.etNameUserRegister)
+        etUserPassword = findViewById(R.id.etPasswordRegister)
+        etUserRepeatPassword = findViewById(R.id.etRepeatPasswordRegister)
+        etUserMail = findViewById(R.id.etEmailRegister)
+        btnRegister = findViewById(R.id.btnRegisterRegister)
+        etUserName.addTextChangedListener {
             userName = etUserName.text.toString()
         }
-        etUserPassword.addTextChangedListener{
+        etUserPassword.addTextChangedListener {
             userPassword = etUserPassword.text.toString()
         }
-        etUserRepeatPassword.addTextChangedListener{
+        etUserRepeatPassword.addTextChangedListener {
             userRepeatPassWord = etUserRepeatPassword.text.toString()
         }
-        etUserMail.addTextChangedListener{
+        etUserMail.addTextChangedListener {
             userMail = etUserMail.text.toString()
         }
 
         btnRegister.setOnClickListener {
-            if(userPassword==userRepeatPassWord){
-                newUser =  User(userName, userPassword,0, userMail)
-                val intent = Intent(this@RegisterActivity, MainActivity::class.java)
-                intent.putExtra("newUser", newUser)
-                startActivity(intent)
-            }else{
+            val readerDb: SQLiteDatabase = recyclerDB.readableDatabase
+            val cursor = readerDb.rawQuery("SELECT name FROM user", null)
+            val listNames = ArrayList<String>()
+            var nameExist = false
+            if (cursor.moveToFirst()) {
+                do {
+                    listNames.add(cursor.getInt(0).toString())
+                } while (cursor.moveToNext())
+            }
+
+            if (userPassword == userRepeatPassWord) {
+
+               listNames.forEach{ name ->
+                   if (name != userName){
+                      nameExist = true
+                   }
+               }
+                if (!nameExist){
+                    newUser = User(userName, userPassword, 0, userMail, 0)
+                    if (newUser.getName() != null && newUser.getPassword() != null && newUser.getMail() != null) {
+                        recyclerDB.setUser(newUser)
+                        Toast.makeText(this, R.string.TxtUserSaveDataBase, Toast.LENGTH_LONG).show()
+                        val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+                        intent.putExtra("newUser", newUser)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, R.string.TxtUserErrorDataBase, Toast.LENGTH_LONG).show()
+                    }
+                }else{
+                    Toast.makeText(this, R.string.TxtNameExists, Toast.LENGTH_LONG).show()
+                    etUserName.setText("")
+
+                }
+
+
+
+            } else {
                 Toast.makeText(this, R.string.TxtErrorPassword, Toast.LENGTH_SHORT).show()
 
             }
